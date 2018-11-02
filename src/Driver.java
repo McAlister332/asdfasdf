5ef6d4d31ca6fa788b5d1df06e08113bdf1b9ce6
McAlister332 <elijah.s.boulton-1@ou.edu>
2018-11-01 20:37:49 -0500

    Created Driver class, no meaningful code inside.

---
+++ src/Driver.java
@@ -0,0 +1,23 @@
+/**
+ * Driver class runs the program.
+ * 
+ * @author Eli
+ * @version 2018-11-01
+ * Project 4
+ */
+public class Driver
+{
+
+   /**
+    * Runs the weather data quantifying program.
+    * 
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+
+      System.out.println("TODO: The whole program...");
+
+   }
+
+}
